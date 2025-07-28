bbd0ede2949546b5430818cced65b0093e4f299b
sam uribe <samuribe@sams-MacBook-Air-2.local>
2025-07-27 23:50:03 -0700

    finsihed constructors for tensor :/

--- include/tensor.h
+++ include/tensor.h
@@ -5,10 +5,11 @@ class tensor{
     private:
         std::vector<float> _data; //not using pointers since vectors dynamically allocate already 
         std::vector<std::size_t> _shape;
+        std::vector<std::size_t> _stride;
 
     public:
-        tensor(float data); //scalar
-        tensor(std::vector<float>); //1D or vector
-        tensor(std::vector<std::vector<float>>); //2D or matrix
-
+        tensor();
+        tensor(float); //scalar
+        tensor(const std::vector<float>&); //1D or vector
+        tensor(const std::vector<std::vector<float>>&); //2D or matrix
 };
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,5 +1,32 @@
 #include "tensor.h"
 #include <iostream>
 
+tensor::tensor(): _data(), _shape(),_stride() {};
+
+tensor::tensor(float data): _data(data), _shape({}), _stride({}){}
+
+tensor::tensor(const std::vector<float>& data): _data(data), _shape({data.size()}), _stride({1}) {}
+
+tensor::tensor(const std::vector<std::vector<float>>& data): _shape({data.size(),data[0].size()}), _stride({data[0].size(),1}) {
+
+    if (data.empty() || data[0].empty()) {
+        throw std::invalid_argument("Cannot initialize tensor with empty data");
+    }
+
+    std::size_t n_expected_columns = data[0].size();
+
+    //check if dimensions match, if each row has the same number of columns//
+    for(const auto& row: data){
+        if(row.size() != n_expected_columns)
+            throw std::invalid_argument("Invalid Dimensions");
+    }
+
+    //store in row major form//
+    for(const auto& i: data){
+        for(const auto& j: i)
+            _data.push_back(j);         
+    }
+
+}
 
 
